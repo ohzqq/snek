d4723afffa03211a74eec3914e451bbe5623d8d8
ohzqq <iamchurkey@gmail.com>
2024-03-05 09:35:58 -0500

    ok

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,10 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -17,16 +15,48 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "snek",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "generate a cli for cobra",
+	Long: `A Cobra companion for quickly generating a cli app with a config file.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		f := args[0]
+		err := readConfig(f)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		d, err := os.Create("cmd/commands.go")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer d.Close()
+
+		_, err = d.WriteString(cfg.Cmds())
+
+		err = fmtCommands("cmd/commands.go")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for name, fn := range cfg.RunFuncs() {
+			n := filepath.Join("cmd", name+".go")
+			f, err := os.Create(n)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer f.Close()
+
+			_, err = f.WriteString(fn)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = fmtCommands(n)
+			if err != nil {
+				log.Fatalf("gofmt err %s\n%s", err, fn)
+			}
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -41,11 +71,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.snek.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "./snek.yaml", "config file (default is ./snek.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
